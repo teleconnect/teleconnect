b7ee6f9924d2562ce4d1e637dca8ecaa64588e02
teleconnect <teleconnectbypunetelstra@gmail.com>
2024-09-26 11:46:19 +0530

    Update AdminDashboardController.java

--- TeleconnectBackend/TeleconnectBackend/src/main/java/com/teleconnect/controller/AdminDashboardController.java
+++ TeleconnectBackend/TeleconnectBackend/src/main/java/com/teleconnect/controller/AdminDashboardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://44.201.255.255")
 @RequestMapping("/api/admin")
 public class AdminDashboardController {
 
